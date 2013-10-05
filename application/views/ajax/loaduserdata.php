64e33f066596f156f164bae3e6587fe1a78b6e2c
Jean-Francois Ouellette <jfr.ouellette@gmail.com>
2013-10-05 23:07:19 +0200

    profile updater deadend

--- application/views/ajax/loaduserdata.php
+++ application/views/ajax/loaduserdata.php
@@ -1,21 +1,28 @@
 <script>
-$('#getdata').click(function(){
- 
-    $.ajax({
-            url: '<?php echo site_url()."/userprofile/loaduserdata" ?>',
-            type:'POST',
-            dataType: 'json',
-            success: function(output_object){
-                $("#result_table").html("");
-                $("#result_table").append("<table class='table'></table>");
-                $.each( output_object, function(key, value){
-                    $("#result_table table").append("<tr><td>" + key + "</td><td>" + value + "</td></tr>");
-                });
-                },   // End of success function of ajax form
-            error: function(error) {
-                    $('#result_table').append('There was an error');
-                } 
-            }); // End of ajax call 
-    
+$('#maindiv').append('<div id="result_table"></div>');
+$.ajax({
+        url: '<?php echo site_url()."/userprofile/loaduserdata" ?>',
+        type:'POST',
+        dataType: 'json',
+        success: function(output_object){
+            $("#result_table").html("");
+            $("#result_table").append("<table class='table'></table>");
+            $.each( output_object, function(key, value){
+                $("#result_table table").append("<tr><td>" + key + "</td><td>" + value + "</td></tr>");
+            });
+            $("#result_table").append('<a href="#" class="btn btn-primary" id="editbtn">Edit</a>');
+            },   // End of success function of ajax form
+        error: function(error) {
+                $('#result_table').append('There was an error');
+            } 
+        });
+     // End of ajax call  
+
+$('#maindiv').on('click', '#editbtn', function( event ){
+    event.preventDefault();
+    console.log("click yeah");
+    $("#result_table table tbody tr").append("<td>form</td>");
+    $("#result_table").append('<button class="btn btn-primary" type="submit" id="save" name="save" value="save">Save</button>');
 });
+
 </script>
